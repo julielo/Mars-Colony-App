068571d43bff515fe14c4411e004608c20a90e80
julielo5 <julielo.inbox@gmail.com>
2017-02-28 15:22:49 -0800

    Styled home page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,10 @@
 @Component({
   selector: 'app-root',
   template: `
-    Mars Colony
-    <a routerLink="/register">Register page</a>
-    <a routerLink="/encounters">Encounters page</a>
-    <a routerLink="/report">Report page</a>
-    <a routerLink="/notfound">Not found page</a>
-    <div class = "page">
-      <router-outlet></router-outlet>
-    </div>
-  `,  
+   <router-outlet></router-outlet>,  
+ 
+`,
+    
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
