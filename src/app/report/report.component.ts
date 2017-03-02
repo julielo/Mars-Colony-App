7aa0deb1a80806a122c8f70ba0f9a39f558ebadc
julielo5 <julielo.inbox@gmail.com>
2017-03-01 19:25:43 -0800

    Created classes for Report page.

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -8,7 +8,26 @@ import { NewEncounter, Alien, Colonist } from '../models';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  newEncounter: NewEncounter;
+  alienType: Alien[];
+  colonist: Colonist[];
+
+  constructor() { 
+
+  this.alienType = [
+    {type: 'Ewok', submitted_by: '3', id: 5, description:'Furry and small'},
+    {type: 'Octospider', submitted_by: '1', id: 2, description: 'Ewww'},
+    {type: 'Endomorph', submitted_by: '2', id: 3, description: 'Doooood'},
+    ];
+
+  this.colonist = [
+    {name: 'Barney', id: 3, age: 43, job: 'Job'},
+    {name: 'Bob', id: 1, age: 35, job: 'job'},
+    {name: 'Jane', id: 2, age: 20, job: 'Job'},
+  ];
+
+  
+  }
 
   ngOnInit() {
   }
